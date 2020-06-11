d567a924e3839b2fad31a4d7f924e75b7c89777a
Daniel Mydlarz <szildaniel@gmail.com>
2020-06-11 21:26:57 +0200

    added another Game test

--- __tests__/Game.test.js
+++ __tests__/Game.test.js
@@ -1,12 +1,19 @@
-import React from "react";
-
 import { render } from "@testing-library/react";
 import Game from '../pages/game';
+import GameTile from '../components/GameTile';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Game', () => {
-    it('shall render without errors', () => {
+    it('should render without errors', () => {
       const {getByText} = render(<Game />)
-      const timerText = getByText(/4/)
-      expect(timerText).toBeInTheDocument();
+      const timerNumber = getByText(/4/)
+      expect(timerNumber).toBeInTheDocument();
     });  
+    it('should render 12 GameTiles components', () => {
+      const wrapper = mount(<Game />);
+      expect(wrapper.find(GameTile)).toHaveLength(12);
+    })
 })
