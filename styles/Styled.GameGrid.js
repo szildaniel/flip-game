0b091672e8af2f550383b8fba8f140979516977a
Daniel Mydlarz <szildaniel@gmail.com>
2020-09-13 17:47:55 +0200

    changed styles for laptop, and more space in game on smarhphones

--- styles/Styled.GameGrid.js
+++ styles/Styled.GameGrid.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { device } from "../styles/breakpoints";
 
 export const GameGrid = styled.div`
-  width: 100vw;
+  width: 90vw;
   height: 55vh;
   position: absolute;
   top: 50%;
@@ -11,8 +11,15 @@ export const GameGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr;
-  @media ${device.laptopL}{
-    width: 60vw;
-    height: 80vh;
+  box-sizing: content-box; 
+  @media ${device.mobileS}{
+    top: 45%;
   }
+  @media ${device.laptop}{
+    width: 60vw;
+  }
+   @media ${device.laptopL} {
+    width: 60vw;
+  };
+  
 `;
